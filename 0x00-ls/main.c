24a25f4dea70c4b4caec3b1d7035ff3d01e282c6
alzheimeer <fogniebla@hotmail.com>
2020-12-01 10:58:27 -0800

    beginning

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -7,6 +7,9 @@
 #include <sys/mman.h>
 #include <dirent.h>
 #include <time.h>
+#include <pwd.h>
+#include <grp.h>
+
 
 int main(int argc, char **argv)
 {
@@ -17,14 +20,35 @@ int main(int argc, char **argv)
 	char *filename = argv[1];
 	dir = opendir(filename);
 	char output[20];
+	struct group *grp;
+	struct passwd *pw;
+	char *time;
 
 	while ((read = readdir(dir)) != NULL)
 	{
 
-		snprintf(output, 20, "%s/%s",filename,read->d_name);
-
 		lstat(output,&sb);
-		printf("%s %ld\n", read->d_name, sb.st_size);
+		printf("%c", (sb.st_mode & S_IFDIR) ? 'd' : '-');
+		printf("%c", (sb.st_mode & S_IRUSR) ? 'r' : '-');
+		printf("%c", (sb.st_mode & S_IWUSR) ? 'w' : '-');
+		printf("%c", (sb.st_mode & S_IXUSR) ? 'x' : '-');
+		printf("%c", (sb.st_mode & S_IRGRP) ? 'r' : '-');
+		printf("%c", (sb.st_mode & S_IWGRP) ? 'w' : '-');
+		printf("%c", (sb.st_mode & S_IXGRP) ? 'x' : '-');
+		printf("%c", (sb.st_mode & S_IROTH) ? 'r' : '-');
+		printf("%c", (sb.st_mode & S_IWOTH) ? 'w' : '-');
+		printf("%c", (sb.st_mode & S_IXOTH) ? 'x' : '-');
+
+		pw = getpwuid(sb.st_uid);
+		grp = getgrgid(sb.st_gid);
+		printf(" %s %s ", pw->pw_name, grp->gr_name);
+		time = ctime(&(sb.st_mtime));
+
+
+		snprintf(output, 20, "%s/%s",filename,read->d_name);
+		printf("%ld %s %s\n", sb.st_size, time, read->d_name);
+
+
 
 
 	}
