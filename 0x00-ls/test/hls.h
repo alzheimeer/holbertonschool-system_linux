75f616e43f0be6e188a41cad136db60ddaf5e35f
alzheimeer <fogniebla@hotmail.com>
2020-12-06 23:40:08 -0500

    angry

--- 0x00-ls/test/hls.h
+++ 0x00-ls/test/hls.h
@@ -15,6 +15,8 @@
 #include <errno.h>
 #include <string.h>
 
+void verFile(char **argv, char *filename);
+
 int basic(char *filename, char **argv,
 		  int pila, int hidden, int longd, int hidden2,
 		  int reverse, int sortsize, int sorttime, int recursion);
@@ -28,6 +30,13 @@ void cases(char **argv, int i, int *pila, int *hidden,
 	int *sorttime, int *recursion);
 
 void flaqs(struct stat sb, struct dirent *read, int pila, int longd,
-	int hidden2, int reverse, int sortsize, int sorttime, int recursion);
+	int reverse, int sortsize, int sorttime, int recursion);
+
+void betty(int i, int argc, int nDir, char **argv,
+	int pila, int hidden, int longd, int hidden2);
+
+int _strcmp(char *s1, char *s2);
+
+char *_strtok(char *s, const char *delim);
 
 #endif /* B79F7134_C774_433E_A9D1_31DF1009321A */
