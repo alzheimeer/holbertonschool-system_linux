77c616af10601857aebb1da2c3af999294e3f400
Alzheimeer <fogniebla@hotmail.com>
2020-12-10 14:56:42 -0500

    task 0

---
+++ 0x01-getline/laps.c
@@ -0,0 +1,60 @@
+#include "laps.h"
+
+
+void race_state(int *id, size_t size)
+{
+    static int cars[64];
+    static int laps[64];
+    static size_t count;
+    int exists, aux;
+    size_t i, j, x;
+    
+    
+    for (i = 0; i < size; i++)
+    {
+        exists = 0;
+        for (j = 0; j < count; j++)
+        {
+            if(id[i] == cars[j])
+                {
+                    exists = 1;
+                    laps[j] += 1;
+                    break;
+                }
+        }    
+        if (exists == 0)
+        {
+            cars[count] = id[i];
+            sort(&cars, &laps, count);
+            
+            count++;
+            printf("Car %d joined the race\n", id[i]);
+        }
+    }
+    printf("Race state:\n");
+    for (j = 0; j < count; j++)
+    {
+        printf("Car %d [%d laps]\n", cars[j], laps[j]);
+    }
+
+}
+
+
+void sort(int *cars, int *laps, int count)
+{
+    int aux;
+    int x;
+    
+    for (x = count; x > 0; x--)
+    {
+        if (cars[x] < cars[x-1])
+            {
+                aux = cars[x-1];
+                cars[x-1] = cars[x];
+                cars[x] = aux;
+                aux = laps[x-1];
+                laps[x-1] = laps[x];
+                laps[x] = aux;
+            }
+    }
+}
--- 0x01-getline/laps.h
+++ 0x01-getline/laps.h
@@ -1,4 +1,9 @@
 #ifndef INC_0X01_GETLINE_LAPS_H
 #define INC_0X01_GETLINE_LAPS_H
+#include <stdio.h>
+
+
+void race_state(int *id, size_t size);
+void sort(int *cars, int *laps, int count);
 
 #endif //INC_0X01_GETLINE_LAPS_H
