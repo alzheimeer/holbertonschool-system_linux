af21d3d19b7f1d7491be414e85251c94b61d0caa
Alzheimeer <fogniebla@hotmail.com>
2020-12-13 23:17:37 -0500

    fix

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -7,44 +7,44 @@
  */
 char *_getline(const int fd)
 {
-	char *line = NULL;
-	static char buf[READ_SIZE] = {0};
-	static int i, j, x;
-	int n = 0, k = 0;
+	char *line = NULL, *buf = NULL;
+	static char *line1;
+	static int i, count_size, j, x;
+	int n = 0, k = 0, n_read = 1, f = 0, size = READ_SIZE;
 
 	if (x == 1)
 	{
-		memset(buf, '$', 1024);
-		i = 0, j = 0, x = 0;
+		free(line1), i = 0, count_size = 0, j = 0, x = 0;
 		return (NULL);
 	}
-	if (i == 0)
-		read(fd, buf, READ_SIZE);
-
-	for (; buf[i] != '\n' && buf[i] != '\0'; i++)
-		;
-
+	buf = malloc(READ_SIZE);
+	while (n_read)
+	{
+		n_read = read(fd, buf, READ_SIZE);
+		if (n_read > 0)
+		{
+			line1 = realloc(line1, size);
+			if (f == 0)
+				memcpy(line1, buf, n_read);
+			else
+				memcpy(line1 + count_size, buf, n_read);
+			size = size + READ_SIZE;	}
+		f++, count_size = n_read + count_size;
+	}
+	while (line1[i] != '\n' && i != count_size)
+		i++;
 	if (i == j)
 		n = 0;
 	else
 		n = i - j;
-
 	line = malloc(n + 1);
-	if (line == NULL)
-		return (NULL);
 	memset(line, 0, n + 1);
-
 	for (; j < i; j++)
-	{
-		line[k] = buf[j];
-		k++;
-	}
-
-	if (buf[i + 1] == '\0')
+		line[k] = line1[j], k++;
+	if (i == count_size)
 		x = 1;
-
 	i = i + 1;
 	j = i;
-
+	free(buf);
 	return (line);
 }
