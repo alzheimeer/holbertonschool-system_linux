78893d08c32335f94900ca0785e4dbfed13d6355
alzheimeer <fogniebla@hotmail.com>
2020-12-14 12:48:35 -0800

    fix Detect fd new , print 3 fd without problem

--- 0x01-getline/_getline.c
+++ 0x01-getline/_getline.c
@@ -11,26 +11,15 @@ char *_getline(const int fd)
 	static char *line1;
 	static int i, count_size, j, x, new;
 	int n = 0, k = 0, n_read = 1, f = 0, size = READ_SIZE;
-	
-	if (fd != new)
-	{
-        free(line1);
-        line1 = NULL;
-        i = 0, count_size = 0, j = 0;
-	}    
-    
-    /*DETECTA QUE LLEGO AL FINAL DEL ARCHIVO*/
-	if (x == 1)
-	{
-		x = 0;
-		return (NULL);
-	}
-	buf = malloc(READ_SIZE);
 
+	if (fd != new)
+		free(line1), line1 = NULL, i = 0, count_size = 0, j = 0;
+	if (x == 1)
+	{	x = 0;
+		return (NULL);	}
+	buf = malloc(READ_SIZE);
 	while (n_read)
-	{
-	    /*LEE EL FD Y MEDIANTE REALLOC Y MEMCPY VAMOS COPIANDO TODO EN LINE1*/
-		n_read = read(fd, buf, READ_SIZE);
+	{	n_read = read(fd, buf, READ_SIZE);
 		if (n_read > 0)
 		{
 			line1 = realloc(line1, size);
@@ -38,58 +27,24 @@ char *_getline(const int fd)
 				memcpy(line1, buf, n_read);
 			else
 				memcpy(line1 + count_size, buf, n_read);
-			size = size + READ_SIZE;
-		}
-		f++;
-		count_size = n_read + count_size;
+			size = size + READ_SIZE;	}
+		f++, count_size = n_read + count_size;
 	}
 	if (n_read == 0)
-	{
-	    new = fd;
-	}
-	
-	/*CUENTA A LINE HASTA EL EL SALTO DE LINEA*/
+		new = fd;
 	while (line1[i] != '\n' && i != count_size)
 		i++;
-	
-	/* SI NO AUMENTA I POR NO EXISTIR NADA*/	
 	if (i == j)
 		n = 0;
-	/* CALCULAMOS CUAN GRANDE ES LA LINEA PARA CREAR EL MALLOC*/
 	else
 		n = i - j;
-		
-	/*CREA LINE Y METE AHI SOLO UNA LINEA DONDE J EMPIEZA Y I TERMINA*/
 	line = malloc(n + 1);
 	memset(line, 0, n + 1);
 	for (; j < i; j++)
 		line[k] = line1[j], k++;
-
-
-
-
-    /*SI LLLEGO AL FINAL DEL TEXTO - YA TENEMOS EN LINE LO QUE FALTA POR DEVOLVER*/
 	if (i == count_size)
-	{
-	    free(line1);
-	    free(buf);
-	    i = 0, count_size = 0, j = 0;
-		x = 1;
-		return (line);
-	}
-
-
-
-
-
-
-
-
-
-    /*ADELANTAMOS EL SALTO DE LINEA E IGUALAMOS I Y J PARA QUE EN LA PROXIMA VUELTA EMPIECE EN J*/
-	i = i + 1, j = i;
-	
-	free(buf);
-	
+	{	free(line1), free(buf), i = 0, count_size = 0, j = 0, x = 1;
+		return (line);	}
+	i = i + 1, j = i, free(buf);
 	return (line);
 }
