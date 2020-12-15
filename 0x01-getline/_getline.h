39d0e9094fc6539cbb3dd7b1bd83b53c78cdbaa3
alzheimeer <fogniebla@hotmail.com>
2020-12-14 21:25:08 -0800

    fix

--- 0x01-getline/_getline.h
+++ 0x01-getline/_getline.h
@@ -1,40 +1,14 @@
-#ifndef _GETLINE_H
-#define _GETLINE_H
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#ifndef INC_0X01_GETLINE_GETLINE_H
+#define INC_0X01_GETLINE_GETLINE_H
+
 #include <unistd.h>
-#define READ_SIZE 512
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-
-
-/**
- * struct STREAM - struct to define each stream source
- *
- * @fd: file descriptor
- * @output: store of actual read data
- * @old_size: stores the old size of the buffer reads
- * @next: pointer to the next stream
- */
-typedef struct STREAM
-{
-	unsigned int fd;
-	unsigned int old_size;
-	char *output;
-	struct STREAM *next;
-} streamf;
+#define READ_SIZE 1024
 
 char *_getline(const int fd);
-int getindex(char *array, char chr);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _strlen(char *s);
-char *split_line(int index);
-void clean_files(void);
-streamf *handlefd(unsigned int fd);
 
-
-
-
-
-
-#endif
+#endif /*INC_0X01_GETLINE_GETLINE_H*/
