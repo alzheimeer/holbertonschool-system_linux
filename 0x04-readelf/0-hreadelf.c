525edfec339499d4886d92ad2e041accca49b688
alzheimeer <fogniebla@hotmail.com>
2021-01-25 09:29:46 -0800

    test first task

--- 0x04-readelf/0-hreadelf.c
+++ 0x04-readelf/0-hreadelf.c
@@ -6,7 +6,6 @@
  * main - read elf file
  * @argc: the count of args
  * @argv: the args
- * @env:  the environment
  * Return: -1 in fail and 0 in success
  */
 int main(int argc, char **argv)
@@ -15,17 +14,17 @@ int main(int argc, char **argv)
 	size_t n = 0;
 	char *line;
 	char args[100];
-	sprintf(args, "/usr/bin/readelf -W -h %s", argv[1]);
 
-   	(void)argc;
-	fp = popen(args,"r");
-	if( fp == NULL)
+	sprintf(args, "/usr/bin/readelf -W -h %s", argv[1]);
+	(void)argc;
+	fp = popen(args, "r");
+	if (fp == NULL)
 	{
 		perror("popen");
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	while (getline(&line, &n, fp) != -1)
-		printf("%s",line);
+		printf("%s", line);
 	pclose(fp);
 	return (EXIT_SUCCESS);
 }
