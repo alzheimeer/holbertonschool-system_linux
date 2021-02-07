441b71a30a81695f6acdf0d7e91ed396e5839b73
alzheimeer <fogniebla@hotmail.com>
2021-02-07 13:41:41 -0800

    fix

--- 0x05-libasm/1-strcmp.asm
+++ 0x05-libasm/1-strcmp.asm
@@ -7,25 +7,25 @@ BITS 64
 asm_strcmp:
 
 while:
-    mov r8b, BYTE [rdi]
-    mov r9b, BYTE [rsi]
-    inc rdi
-	inc rsi
-    cmp r8b, r9b
-    jl menor
-    jg mayor
-	cmp r8b, 0
-    je igual
-    jmp while
+    mov r8b, BYTE [rdi];pasamos a registro de 8 bit
+    mov r9b, BYTE [rsi];pasamos a registro de 8 bit
+    inc rdi;pasamos a la siguiente letra
+	inc rsi;pasamos a la siguiente letra
+    cmp r8b, r9b;comparamos las dos letras
+    jl menor;salta si es menor A<B (con signo)
+    jg mayor;salta si mayor A>B (con signo)
+	cmp r8b, 0;verificamos que no haya llegado al final de el string
+    je igual;salta si igual
+    jmp while; salta a while:
 
 mayor:
-    mov eax, 1
-    ret
+    mov eax, 1;seteamos eax con 1
+    ret; salida
 
 menor:
-    mov eax, -1
-    ret
+    mov eax, -1;seteamos eax con -1
+    ret; salida
 
 igual:
-	mov eax, 0
-    ret
+	mov eax, 0;seteamos eax con 0
+    ret; salida
