4f22ac6a8c04c36920242ef3deca8d74e86d4601
alzheimeer <fogniebla@hotmail.com>
2021-02-07 13:17:07 -0800

    fix checkers

--- 0x05-libasm/1-strcmp.asm
+++ 0x05-libasm/1-strcmp.asm
@@ -6,12 +6,12 @@ BITS 64
 asm_strcmp:
 
 while:
-	mov dil, [rdi] ;pasamos a registro de 8 bit
-	mov sil, [rsi] ;pasamos a registro de 8 bit
-	cmp dil, sil ;comparamos las dos letras
+	mov r8b, [rdi] ;pasamos a registro de 8 bit
+	mov r9b, [rsi] ;pasamos a registro de 8 bit
+	cmp r8b, r9b ;comparamos las dos letras
 	jl menor ;salta si es menor A<B (con signo)
 	jg mayor ;salta si mayor A>B (con signo)
-	cmp dil, 0 ;verificamos que la letra no sea 0
+	cmp r8b, 0 ;verificamos que la letra no sea 0
 	je igual ;salta si igual	A=B
 	inc rdi ;pasamos a la siguiente letra
 	inc rsi ;pasamos a la siguiente letra
