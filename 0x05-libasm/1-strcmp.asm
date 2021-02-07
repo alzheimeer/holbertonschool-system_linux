e170519701306bfa204142f13743577928c137c9
alzheimeer <fogniebla@hotmail.com>
2021-02-07 13:37:30 -0800

    fix

--- 0x05-libasm/1-strcmp.asm
+++ 0x05-libasm/1-strcmp.asm
@@ -6,39 +6,26 @@ BITS 64
 
 asm_strcmp:
 
-           
-
-loop_strcmp:
-    mov r8b, BYTE [rdi + rcx]
-    mov r9b, BYTE [rsi + rcx]
+while:
+    mov r8b, BYTE [rdi]
+    mov r9b, BYTE [rsi]
     inc rdi
 	inc rsi
-    test r8b, r8b
-    je check_null              
-    test r9b, r9b
-    je set_one               
     cmp r8b, r9b
-    je loop_strcmp             
-    jmp compare
+    jl menor
+    jg mayor
+	cmp r8b, 0
+    je igual
+    jmp while
 
-check_null:
-    test r9b, r9b
-    je end_strcmp
-    jmp set_negative
-
-compare:
-    cmp r8b, r9b
-    jl set_negative
-    jmp set_one
-
-set_one:
+mayor:
     mov eax, 1
     ret
 
-set_negative:
-    mov eax, -1            
+menor:
+    mov eax, -1
     ret
 
-end_strcmp:
+igual:
 	mov eax, 0
     ret
