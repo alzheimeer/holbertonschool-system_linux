35d8e3ad9ee7b0f274d8b01ceffc9abf390b93dd
alzheimeer <fogniebla@hotmail.com>
2021-02-07 16:49:37 -0800

    task 6

---
+++ 0x05-libasm/6-putc.asm
@@ -0,0 +1,16 @@
+  
+BITS 64
+
+    global asm_putc
+
+    section .text
+
+asm_putc:
+    mov rax, 1                  ; write syscall
+    mov rsi, rsp                ; Character
+    mov rdi, 1                  ; stdout
+    mov rdx, 1                  ; one byte
+    syscall
+
+end:
+    ret
