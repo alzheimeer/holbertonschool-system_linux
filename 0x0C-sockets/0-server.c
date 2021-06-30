2d66dc974da26b51da11da988b09ed28d017d184
alzheimeer <fogniebla@hotmail.com>
2021-06-30 01:01:41 -0500

    fix

--- 0x0C-sockets/0-server.c
+++ 0x0C-sockets/0-server.c
@@ -18,23 +18,23 @@ void err_socket(char *msg)
  */
 int main(void)
 {
-	int socket_server = 0, server_size = 0;
-	struct sockaddr_in addr_server;
+	int sockid = 0, server_size = 0;
+	struct sockaddr_in addrport;
 
 	/*Create the file descriptor for a socket server*/
-	socket_server = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (socket_server == -1)
+	sockid = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sockid == -1)
 		err_socket("socket failed");
 	/*Bind the socket with a port and an address in ipv4*/
-	addr_server.sin_family = AF_INET;
-	addr_server.sin_port = htons(PORT);
-	addr_server.sin_addr.s_addr = htonl(INADDR_ANY);
+	addrport.sin_family = AF_INET; /* Internet address family */
+	addrport.sin_port = htons(PORT); /* Local port */
+	addrport.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
 
-	server_size = sizeof(addr_server);
-	if (bind(socket_server, (struct sockaddr *) &addr_server, server_size) == -1)
+	server_size = sizeof(addrport);
+	if (bind(sockid, (struct sockaddr *) &addrport, server_size) == -1)
 		err_socket("bind failed");
 	/*Let the listening of the socket with 10 waiting clients*/
-	if (listen(socket_server, NUM_CLNTS) == -1)
+	if (listen(sockid, NUM_CLNTS) == -1)
 		err_socket("listen failed");
 	/*Waiting a client connection*/
 	printf("Socket server listen on port %d\n", PORT);
